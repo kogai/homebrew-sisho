3361572a8f047f32cd29408070332fba78a85f4f
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2016-01-24 21:29:10 +0900

    Update version

--- sisho.rb
+++ sisho.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
-TOOL_VERSION = '0.1.4'
+TOOL_VERSION = '0.1.5'
 
 class Sisho < Formula
 
   url "https://github.com/gophergala2016/sisho/releases/download/v#{TOOL_VERSION}/sisho"
-  sha1 "b18e09a66a36645775b4eef7eca41c2038930694"
+  sha1 "f41d0f4dd351715fec3f43b4abd86af7633caf50"
   version "v#{TOOL_VERSION}"
 
   def install
