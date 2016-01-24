692f728cbc413fdd2ccf36909a0ad650b0b0053c
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2016-01-24 15:24:03 +0900

    Fix SHA

--- sisho.rb
+++ sisho.rb
@@ -5,7 +5,7 @@ TOOL_VERSION = '0.1.2'
 class Sisho < Formula
 
   url "https://github.com/gophergala2016/sisho/releases/download/v#{TOOL_VERSION}/sisho"
-  sha1 "0d22f77af72d40b1c79f53fae84df7bbae4f73c2"
+  sha1 "e0b9631e0d67cf73ef42b764e1cfc79a1aa28370"
   version "v#{TOOL_VERSION}"
 
   def install
